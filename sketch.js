f71cc328e7b261e67703d1fad3ff9f7767406120
Safa1117 <73911822+Safa1117@users.noreply.github.com>
2020-12-22 17:49:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground;
+var paper;
+var dustbin;
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(800,670,width,20);
+
+	paper = new Paper(200,450,40);
+
+	dustbin = new Dustbin(1200,650);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ground.display();
+  paper.display();
+  dustbin.display();
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	  Matter.Body.applyForce(paper.body, paper.body.position, {x:85, y:-85});
+	 }
+   }
+
+
